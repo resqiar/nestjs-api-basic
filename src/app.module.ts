e6997d4af5dc2c72e20803681a465eaffa4d2e7d
resqiar <resqiageng@gmail.com>
2021-03-29 15:51:51 +0700

    Logging Interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { DatabaseModule } from './database/database.module'
-import { APP_FILTER } from '@nestjs/core'
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
 import { HttpErrorFilter } from './shared/http-error.filter'
+import { LoggingInterceptor } from './shared/logging.interceptor'
 
 @Module({
 	imports: [UsersModule, DatabaseModule],
@@ -20,6 +21,15 @@ import { HttpErrorFilter } from './shared/http-error.filter'
 			provide: APP_FILTER,
 			useClass: HttpErrorFilter,
 		},
+    /**
+     * This providers is going to intercept
+     * Any HttpRequest and log everything
+     * Log message is defined in LoggingInterceptor
+     */
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor
+    }
 	],
 })
 export class AppModule {}
